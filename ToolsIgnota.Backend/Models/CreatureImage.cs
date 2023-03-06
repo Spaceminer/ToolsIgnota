91024cfc7cb08ae749d420ff02c1dca00cc22e2d
Ryan Sherman <rsherman@hsi.com>
2023-03-05 21:15:30 -0800

    Wiped project

--- ToolsIgnota.Backend/Models/CreatureImage.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace ToolsIgnota.Data.Models
-{
-    public class CreatureImage
-    {
-        public string Name { get; set; } = "";
-        public string Image { get; set; } = "..";
-    }
-}
